1c042f35e3a4eb9d4b252270ced230199fd2a51e
Francisco Quintero Ubuntu <frajaquico@yahoo.es>
2014-09-13 18:52:45 -0500

    cambio de archivos a la carpeta opensearcher

---
+++ platforms/android/ant-build/OpenSearcher-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/var/www/opensearcher/os/platforms/android/ant-build/OpenSearcher-debug-unaligned.apk : \
+/var/www/opensearcher/os/platforms/android/ant-build/OpenSearcher.ap_ \
+/var/www/opensearcher/os/platforms/android/ant-build/classes.dex \
+/var/www/opensearcher/os/platforms/android/CordovaLib/ant-build/classes.jar \
